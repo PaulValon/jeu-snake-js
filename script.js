06e40efe9ec7cf5090c2ee7f74e55aee5df54f51
THOMAS MAIRE <thomas.maire@cheops.fr>
2021-04-28 15:06:06 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+window.onload = function()
+{
+  let canvas;
+  let ctx;
+  let delay = 100;
+  let xCoord = 0;
+  let yCoord = 0;
+
+
+  init();
+
+  function init()
+  {
+    canvas = document.createElement('canvas');
+    canvas.width = 900;
+    canvas.height = 600;
+    canvas.style.border = "1px solid";
+    document.body.appendChild(canvas);
+    ctx = canvas.getContext('2d');
+    refreshCanvas();
+
+  }
+
+  function refreshCanvas()
+  {
+    xCoord += 2;
+    yCoord += 2;
+    ctx.clearRect(0 ,0 ,canvas.width ,canvas.height);
+    ctx.fillStyle = "#ff0000";
+    ctx.fillRect(xCoord , yCoord , 100, 50);
+    setTimeout(refreshCanvas,delay);
+  }
+  
+}
